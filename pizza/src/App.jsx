4033b3964aa88c411fa0162263084b4387db503a
Леванов Р.Г. <r.levanov@nordsy.spb.ru>
2025-03-05 17:14:11 +0300

    first commit

---
+++ pizza/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './scss/app.scss'
+import Header from "./components/Header.jsx";
+import Categories from "./components/Categories.jsx";
+import Sort from "./components/Sort.jsx";
+import PizzaBlock from "./components/PizzaBlock.jsx";
+import pizzas from "./assets/pizzas.json";
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {
+                            pizzas.map((obj) => <PizzaBlock
+                                                    title={obj.title}
+                                                    price={obj.price}
+                                                    imageUrl={obj.imageUrl}
+                                                    sizes={obj.sizes}
+                                                    types={obj.types}
+                                                />)
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default App
---
+++ pizza/src/components/Categories.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+export default function Categories() {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index);
+    } // можно сократить как в PizzaBlock
+    return (
+        <div className="categories">
+            <ul>
+                {
+                    categories.map((value, index) => (
+                        <li onClick={() => onClickCategory(index)} className={activeIndex === index ? 'active' : ''}>
+                            {value}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
